81b5730b09f57f46296513ea40d8f05ad27c9da9
Samuel White <148637178+MostAmbitiousCrow@users.noreply.github.com>
2024-10-29 17:18:12 +0000

    Player Turret AI Update

--- Assets/Protoytpe Assets/Scripts/Player_Turret.cs
+++ Assets/Protoytpe Assets/Scripts/Player_Turret.cs
@@ -4,15 +4,134 @@ using UnityEngine;
 
 public class Player_Turret : MonoBehaviour
 {
+    [Header("Stats")]
+    [Tooltip("Time each projectile is fired")]
+    [SerializeField] private float fireRate = 1;
+    [SerializeField] private float turnSpeed = 1;
+    [SerializeField] private float damage = 1;
+
+    [Header("Components")]
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Transform projectileSpawn;
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] Transform body;
+
+    [SerializeField] List<GameObject> cachedProjectiles;
+
+    [Header("Target Info")]
+    [SerializeField] private bool emptyTargets = true;
+    [SerializeField] Transform currentTarget;
+    [SerializeField] List<Transform> detectedTargets;
+
+    public enum TurretState { First, Last, Closest, Strongest }
+    [SerializeField] TurretState turretState = TurretState.First;
+    private TurretState currentState;
+
+    private Coroutine currentRoutine;
+    private int targetCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateState(turretState);
     }
 
     // Update is called once per frame
-    void Update()
+    public void UpdateState(TurretState state)
     {
-        
+        turretState = state;
+        if (targetCount == 0) return;
+        switch (state)
+        {
+            case TurretState.First:
+                currentTarget = detectedTargets[0];
+                BeginFiring(TurretState.First);
+                break;
+            case TurretState.Last:
+                currentTarget = detectedTargets[targetCount - 1];
+                BeginFiring(TurretState.Last);
+                break;
+            case TurretState.Closest:
+                currentTarget = FindClosestTarget();
+                BeginFiring(TurretState.Closest);
+                break;
+            case TurretState.Strongest: // Maybe leave this. Could be too complex?
+
+                BeginFiring(TurretState.Strongest);
+                break;
+        }
+    }
+
+    // Credit https://discussions.unity.com/t/clean-est-way-to-find-nearest-object-of-many-c/409917
+    Transform FindClosestTarget()
+    {
+        float minDist = Mathf.Infinity;
+        Vector3 pos = transform.position;
+
+        Transform target = null;
+        foreach (var item in detectedTargets)
+        {
+            float dist = Vector3.Distance(pos, item.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                target = item;
+            }
+        }
+        Debug.Log(gameObject.name + "'s Nearest Target is " + target.name);
+        return target;
+    } // Find closest enemy in list
+
+    private void BeginFiring(TurretState state)
+    {
+        if (state == turretState)
+        {
+            return;
+        }
+        if (currentRoutine != null) StopCoroutine(currentRoutine);
+        currentRoutine = StartCoroutine(FiringRoutine());
+    }
+
+    private IEnumerator FiringRoutine() // Where the action takes place
+    {
+        float t = 0;
+        Vector3 pos = transform.position;
+        Quaternion rot = Quaternion.identity;
+        while (!emptyTargets)
+        {
+            float yRot = Quaternion.LookRotation(currentTarget.position - pos).eulerAngles.y;
+            rot = Quaternion.RotateTowards(body.rotation, Quaternion.Euler(0, yRot, 0), turnSpeed);
+            body.rotation = rot;
+
+            t += Time.deltaTime;
+            if (t > fireRate)
+            {
+                //Instantiate() // Unfinished
+            }
+
+            yield return null;
+        }
+        yield break;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (emptyTargets)
+        {
+            emptyTargets = false;
+            BeginFiring(turretState); // Continue firing
+        }
+        targetCount += 1;
+        detectedTargets.Add(other.transform);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!emptyTargets)
+        {
+            emptyTargets = true;
+            StopCoroutine(currentRoutine); // Stop firing
+        }
+        targetCount = -1;
+        detectedTargets.Remove(other.transform);
     }
 }
